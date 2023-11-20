2b1d61de0cbbada5f1a5d4ffa47d410dc1cff013
Pravar <pravargupta2@gmail.com>
2023-11-20 23:53:05 +0530

    small change in frontend

--- client/src/api/PostsRequests.js
+++ client/src/api/PostsRequests.js
@@ -1,15 +1,14 @@
-import axios from 'axios'
-
+import axios from 'axios';
 
 const API = axios.create({ baseURL: 'https://mern-backend-final-nw6v.onrender.com/' });
 
 API.interceptors.request.use((req) => {
-    if (localStorage.getItem('profile')) {
-      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-    }
-  
-    return req;
-  });
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+  }
 
-export const getTimelinePosts= (id)=> API.get(`/posts/${id}/timeline`);
-export const likePost=(id, userId)=>API.put(`posts/${id}/like`, {userId: userId})
+  return req;
+});
+
+export const getTimelinePosts = (id) => API.get(`/posts/${id}/timeline`);
+export const likePost = (id, userId) => API.put(`/posts/${id}/like`, { userId: userId }); // Added a forward slash (/)
